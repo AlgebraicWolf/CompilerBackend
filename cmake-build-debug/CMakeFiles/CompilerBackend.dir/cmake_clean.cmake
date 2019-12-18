5bec4123ca084bb5a1b718f722ea1017a3d37941
Alexey Volkov <volkov.aa@phystech.edu>
2019-12-19 00:03:35 +0300

    Created Project

---
+++ cmake-build-debug/CMakeFiles/CompilerBackend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompilerBackend.dir/main.cpp.o"
+  "CompilerBackend"
+  "CompilerBackend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompilerBackend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CompilerBackend.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CompilerBackend.
+# This may be replaced when dependencies are built.
